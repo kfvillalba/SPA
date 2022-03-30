733b5844d0b54713b161fed10b43cd54713488c1
kfvillalba <70710723+kfvillalba@users.noreply.github.com>
2022-03-30 08:59:30 -0500

    java

---
+++ app.js
@@ -0,0 +1,5 @@
+/* Aplicacion para guardas datos en la tabla*/
+// Declaracion de variables
+var Nombre, Apellido, Telefono, Email, Direccion, Fecha;
+var tabla = document.getElementById("Tabla_Datos_Personales");
+var 
